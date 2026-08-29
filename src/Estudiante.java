b689fe1c0efd80a9d74c2b5c3e80cdfcee3535b2
Ivan Dev~ <jorgecastaneda.ia20@gmail.com>
2026-08-28 20:34:31 -0500

    Segundo comit: Se agrega constructor sobrecargado

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -10,4 +10,16 @@ public class Estudiante {
             this.carrera = "Ingenieria de Software";
             System.out.println(">> Se creó un Estudiante con el constructor POR DEFECTO");
         }
+        // Constructor con parámetros sobrecargado
+        public Estudiante(String nombre, int edad, String carrera) {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.carrera = carrera;
+            System.out.println(">> Se creó un Estudiante con el constructor PARAMETRIZADO");
     }
+
+        public void mostrarInfo() {
+            System.out.println("Estudiante: " + nombre + " / Edad: " + edad + " / Carrera: " + carrera);
+    }
+}
+
