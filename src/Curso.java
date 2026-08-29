c5272e04a0f972cdbc5e17e7ca21a7b00663da33
Ivan Dev~ <jorgecastaneda.ia20@gmail.com>
2026-08-28 21:01:48 -0500

    Septimo comit: Se crea constructor 1 encargado de recibir nombre curso

--- src/Curso.java
+++ src/Curso.java
@@ -2,4 +2,11 @@ public class Curso {
     private String nombreCurso;
     private int cupos;
     private String profesorAsignado;
-}
+
+    //Constructor 1 recibe nombre del curso
+    public Curso(String nombreCurso) {
+        this(nombreCurso, 20); // le manda un valor fijo de 20 cupos al constructor 2
+        System.out.println(">> Constructor 1: se asignó cupo por defecto (20)");
+    }
+
+
