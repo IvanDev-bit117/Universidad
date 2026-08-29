618525db58916b3e9ea04ae75c9f37f6207500c8
Ivan Dev~ <jorgecastaneda.ia20@gmail.com>
2026-08-28 20:27:16 -0500

    Primer comit: Creacion de la clase estudiante con atributos y un constructor sin parametros

---
+++ src/Estudiante.java
@@ -0,0 +1,13 @@
+public class Estudiante {
+        private String nombre="";
+        private int edad=0;
+        private String carrera"";
+
+        // Constructor sin parámetros
+        public Estudiante() {
+            this.nombre = "Jorge Castañeda";
+            this.edad = 21;
+            this.carrera = "Ingenieria de Software";
+            System.out.println(">> Se creó un Estudiante con el constructor POR DEFECTO");
+        }
+    }
