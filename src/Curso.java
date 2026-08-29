bd339e525d6ee072972924c635079c726a9f1769
Ivan Dev~ <jorgecastaneda.ia20@gmail.com>
2026-08-28 21:02:49 -0500

    Octavo comit: Se crea constructor 2 encargado de recibir nombre curso y cupos

--- src/Curso.java
+++ src/Curso.java
@@ -8,5 +8,9 @@ public class Curso {
         this(nombreCurso, 20); // le manda un valor fijo de 20 cupos al constructor 2
         System.out.println(">> Constructor 1: se asignó cupo por defecto (20)");
     }
-
+    // Constructor 2: recibe nombre y cupos mediante this
+    public Curso(String nombreCurso, int cupos) {
+        this(nombreCurso, cupos, "Sin profesor asignado"); // manda un profesor fijo
+        System.out.println(">> Constructor 2: se asignó profesor por defecto");
+    }
 
